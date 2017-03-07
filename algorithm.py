f0aac3409435ccd7cfcffc60f2667d3ea2604e11
Khurram Javed <14besekjaved@seecs.edu.pk>
2017-03-07 21:48:31 +0500

    Pushing demo

--- algorithm.py
+++ algorithm.py
@@ -9,20 +9,26 @@ img = cv2.imread("/home/khurramjaved/Dicta/Test_data/1/paper001.avi/025.jpg",1)
 
 result =np.copy(img)
 data  =getcorners.get_corners(img)
+tf.reset_default_graph()
+model = corner_refinement.corner_finder()
 corner_address=[]
+import timeit
+import time
+start = timeit.timeit()
+start = time.clock()
 for b in data:
-
     a = b[0]
 
-    print a.shape
-    tf.reset_default_graph()
-    temp = np.array(corner_refinement.get_location(a))
-
+    temp = np.array(model.get_location(a))
     temp[0]+= b[1]
     temp[1]+= b[2]
     corner_address.append(temp)
+    print temp
 
-#img = cv2.resize(img, (800,800))
+
+
+end = time.clock()
+print "TOTAL TIME : ", end - start
 for a in range(0,len(data)):
     cv2.line(img, tuple(corner_address[a%4]), tuple(corner_address[(a+1)%4]),(255,0,0),2)
 
