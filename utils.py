af2a24e766035ec078101a48dd43fc4f69c87a16
Khurram Javed <14besekjaved@seecs.edu.pk>
2017-03-17 22:01:10 +0500

    Adding new calculator

--- utils.py
+++ utils.py
@@ -126,4 +126,44 @@ def intersection(a,b, img):
     print np.sum(inte)
     print np.sum(union)
 
+def intersection_with_corection(a,b,img):
+    img1 = np.zeros_like(img)
+    cv2.fillConvexPoly(img1,a,(255,0,0))
+    # cv2.fillConvexPoly(img1,a,(255,0,0))
+
+    img2 = np.zeros_like(img)
+    cv2.fillConvexPoly(img2,b,(255,0,0))
+    min_x = min(a[0][0],a[1][0],a[2][0],a[3][0])
+    min_y = min(a[0][1],a[1][1],a[2][0],a[3][0])
+    max_x = max(a[0][0],a[1][0],a[2][0],a[3][0])
+    max_y = max(a[0][1],a[1][1],a[2][0],a[3][0])
+
+    dst = np.array(((min_x, min_y), (max_x, min_y), (max_x, max_y), (min_x, max_y)))
+    mat = cv2.getPerspectiveTransform(a.astype(np.float32), dst.astype(np.float32))
+    img1 = cv2.warpPerspective(img1, mat,tuple((img.shape[0],img.shape[1])))
+    img2 = cv2.warpPerspective(img2, mat,tuple((img.shape[0],img.shape[1])))
+    cv2.imwrite("../temp.jpg", img1);
+    cv2.imwrite("../temp1.jpg", img2);
+
+    img1 =np.sum(img1,axis=2)
+    img1=img1/255
+    img2 =np.sum(img2, axis=2)
+    img2 = img2 / 255
+
+    inte = img1*img2
+    union = np.logical_or(img1, img2)
+    iou = np.sum(inte)/np.sum(union)
+    print iou
+    return iou
+    
+
+if __name__ == "__main__":
+    img = cv2.imread("/home/khurramjaved/Dicta/Data/background01/magazine003.avi/125.jpg")
+
+
+    a = np.array(((653,234), (1027,312), (885,816), (430, 712))) 
+    b = np.array(((646,237),(1023, 312), (887,814), (431,710)))
+    intersection(a,b, img)
+    intersection_with_corection(a,b,img)
+
 
