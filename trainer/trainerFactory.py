a975146ad0d6880c62816a4f7eb09cb2e7ec8a7a
khurramjaved96 <14besekjaved@seecs.edu.pk>
2017-12-07 17:52:13 +0500

    Removing segmentation network trainer

--- trainer/trainerFactory.py
+++
@@ -1,8 +0,0 @@
-import trainer
-class trainerFactory:
-    @staticmethod
-    def getTrainer(type,trainDir, validateDir, checkpointDir, concatinateFeatures):
-        if type=="documentDetector":
-            return trainer.documentDetector(trainDir, validateDir, checkpointDir, concatinateFeatures = concatinateFeatures)
-        elif type=="cornerDetector":
-            return trainer.cornerDetector
