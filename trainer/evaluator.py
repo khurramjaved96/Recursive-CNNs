7443863ef78a23541b105a35b71ed2f34a850acf
khurramjaved96 <kjaved@ualberta.ca>
2018-06-03 00:47:00 +0500

    Shifting to MSE again

--- trainer/evaluator.py
+++ trainer/evaluator.py
@@ -48,7 +48,7 @@ class DocumentMseEvaluator():
             response = model(Variable(img))
             # print (response[0])
             # print (target[0])
-            loss = F.l1_loss(response, Variable(target.float()))
+            loss = F.mse_loss(response, Variable(target.float()))
             if lossAvg is None:
                 lossAvg = loss
             else:
