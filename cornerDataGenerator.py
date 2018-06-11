0812825b28dc646178baef01bf1ed4929b516459
khurramjaved96 <kjaved@ualberta.ca>
2018-06-11 23:06:01 +0500

    Adding refactor cornerDataGeneration file

---
+++ cornerDataGenerator.py
@@ -0,0 +1,54 @@
+import os
+import cv2
+import numpy as np
+from PIL import Image
+
+import DataLoader.dataset as dataset
+from utils import utils
+
+
+def args_processor():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-dir", help="Path to data files (Extract images using video_to_image.py first")
+    parser.add_argument("-o", "--output-dir", help="Directory to store results")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = args_processor()
+    input_directory = args.input_dir
+    if not os.path.isdir(args.output_dir):
+        os.mkdir(args.output_dir)
+    import csv
+
+    # Dataset iterator
+    dataset_test = dataset.SmartDocDirectories(input_directory)
+    with open(args.outputFiles + 'gt.csv', 'a') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        # Counter for file naming
+        counter = 0
+        for data_elem in dataset_test.myData:
+
+            img_path = data_elem[0]
+            target = data_elem[1].reshape((4, 2))
+            img_array = np.array(Image.open(img_path))
+            img = cv2.imread(img_path)
+            corner_cords = target
+
+            for angle in range(0, 1):
+                img_rotate, gt_rotate = utils.rotate(img, corner_cords, angle)
+                for random_crop in range(0, 1):
+                    img_list, gt_list = utils.get_corners(img_rotate, gt_rotate)
+                    for a in range(0, 4):
+                        counter += 1
+                        f_name = str(counter).zfill(8)
+                        print(gt_list[a])
+                        gt_store = list(np.array(gt_list[a]) / (300, 300))
+                        img_store = cv2.resize(img_list[a], (64, 64))
+                        # cv2.circle(img_store, tuple(list((np.array(gt_store)*64).astype(int))), 2, (255, 0, 0), 2)
+
+                        cv2.imwrite(args.outputFiles + f_name + str(a) + str(angle) + str(random_crop) + ".jpg",
+                                    img_store, [int(cv2.IMWRITE_JPEG_QUALITY), 80])
+                        spamwriter.writerow((f_name + str(a) + str(angle) + str(random_crop) + ".jpg", tuple(gt_store)))
